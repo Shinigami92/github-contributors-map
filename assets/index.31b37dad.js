b46f23078553efe39c4b72ae014a8b4fb0d49da1
Shinigami92 <chrissi92@hotmail.de>
2021-07-24 14:44:04 +0200

    deploy

---
+++ assets/index.31b37dad.js
@@ -0,0 +1,1 @@
+import{d as r,c as a,o as n,a as o}from"./vendor.31197477.js";var t=r({name:"App"});t.render=function(r,o,t,p,s,u){return n(),a("div",null,"GitHub Contributors Map")},o(t).mount("#app");
